5bc2fa7876386b0b0cefb0fcb78ff750abbc8215
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2025-02-23 00:13:09 +0000

    Refactor VEFluid class for improved code readability and consistency

--- src/fluid.py
+++ src/fluid.py
@@ -4,27 +4,29 @@ import data as Data
 import HashGrid
 import math
 
+
 class VEFluid:
-    def __init__(self, particle_count : int, boundary_box: Tuple[float, float]):
+    def __init__(self, particle_count: int, boundary_box: Tuple[float, float]):
         self.particle_count = particle_count
-        self.boundary_box = boundary_box 
+        self.boundary_box = boundary_box
         self.velocity_damping = 1
         self.REST_DENSITY = 2
         self.K_NEAR = 3
         self.K = 0.5
-        self.INTERACTION_RADIUS = 1.5 # can be a float
+        self.INTERACTION_RADIUS = 1.5  # can be a float
         self.INTERACTION_RADIUS_2 = self.INTERACTION_RADIUS**2
-        self.GRAVITY : Data.Vector2 = Data.Vector2(0, 0.2)
+        self.GRAVITY: Data.Vector2 = Data.Vector2(0, 0.2)
         self.particles: List[Data.Particle] = self.initialize_particlesB()
-        self.fluidHashGrid = HashGrid.FluidHashGrid(self.INTERACTION_RADIUS, self.particles)
-        
+        self.fluidHashGrid = HashGrid.FluidHashGrid(
+            self.INTERACTION_RADIUS, self.particles)
 
     def neighbourSearch(self, mousePos):
         self.fluidHashGrid.clearGrid()
         self.fluidHashGrid.mapParticleToCell()
 
-        mouseVector2 = Data.Vector2(mousePos[0], mousePos[1])*(1/8) # i.e. 1/scale factor established on main
-        
+        # i.e. 1/scale factor established on main
+        mouseVector2 = Data.Vector2(mousePos[0], mousePos[1])*(1/8)
+
         hashGridID = self.fluidHashGrid.getGridHashFromPosition(mouseVector2)
         contentOfCell = self.fluidHashGrid.getContentOfCell(hashGridID)
         for p in self.particles:
@@ -32,29 +34,31 @@ class VEFluid:
 
         if contentOfCell != None:
             for p in contentOfCell:
-                p.colour = (255,200,0)
+                p.colour = (255, 200, 0)
 
-        
     def neighbourSearchB(self, mousePos):
         self.fluidHashGrid.clearGrid()
         self.fluidHashGrid.mapParticleToCell()
 
-        mouseVector2 = Data.Vector2(mousePos[0], mousePos[1])*0.2 # i.e. 1/scale factor established on main
-       
+        # i.e. 1/scale factor established on main
+        mouseVector2 = Data.Vector2(mousePos[0], mousePos[1])*0.2
+
         self.particles[0].position = mouseVector2
-        cellsAroundMouse: List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(0)
+        cellsAroundMouse: List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(
+            0)
 
         self.resetParticleColours()
 
         if len(cellsAroundMouse) > 0:
             for p in cellsAroundMouse:
-                distanceSquared = (p.position - mouseVector2).magnitudeSquared()
-                p.colour = (255,200,0) if distanceSquared < self.INTERACTION_RADIUS_2 else p.colour
+                distanceSquared = (
+                    p.position - mouseVector2).magnitudeSquared()
+                p.colour = (
+                    255, 200, 0) if distanceSquared < self.INTERACTION_RADIUS_2 else p.colour
 
     def neighbourSearchC(self):
         self.fluidHashGrid.clearGrid()
         self.fluidHashGrid.mapParticleToCell()
-
 
     def initialize_particles(self) -> List[Data.Particle]:
         particles: List[Data.Particle] = []
@@ -69,27 +73,27 @@ class VEFluid:
         sideLength = round(math.sqrt(self.particle_count))
         for i in range(sideLength):
             for j in range(sideLength):
-                x = i + 32
+                x = i
                 y = j
                 particles.append(Data.Particle(x, y))
         return particles
 
-    def predict_positions(self, dt:float):
+    def predict_positions(self, dt: float):
         for p in self.particles:
             p.prevPosition = p.position
             damped_time = dt*self.velocity_damping
             positionDelta: Data.Vector2 = p.velocity * damped_time
             p.position = p.position + positionDelta
-    
+
     def applyGravity(self, dt):
         for p in self.particles:
             p.velocity = p.velocity + (self.GRAVITY * dt)
 
-
     def compute_next_velocity(self, dt):
         inverse_dt = 1.0/dt
         for p in self.particles:
-            velocity: Data.Vector2 = (p.position - p.prevPosition) * (inverse_dt)
+            velocity: Data.Vector2 = (
+                p.position - p.prevPosition) * (inverse_dt)
             p.velocity = velocity
 
     def resetParticleColours(self):
@@ -100,7 +104,8 @@ class VEFluid:
         for i in range(len(self.particles)):
             density = 0
             densityNear = 0
-            neighbours : List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(i)
+            neighbours: List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(
+                i)
             particle_A = self.particles[i]
 
             for n in neighbours:
@@ -112,9 +117,9 @@ class VEFluid:
                 if q < 1.0:
                     density += math.pow(1-q, 2)
                     densityNear += math.pow(1-q, 3)
-            pressure = self.K* (density - self.REST_DENSITY)
+            pressure = self.K * (density - self.REST_DENSITY)
             pressureNear = self.K_NEAR * densityNear
-            particleADisplacement = Data.Vector2(0,0)
+            particleADisplacement = Data.Vector2(0, 0)
 
             for n in neighbours:
                 particle_B = n
@@ -125,7 +130,8 @@ class VEFluid:
 
                 if q < 1.0:
                     r_ij.normalize()
-                    displacementTerm : float = math.pow(dt, 2)*(pressure * (1-q) + pressureNear*math.pow((1-q), 2))
+                    displacementTerm: float = math.pow(
+                        dt, 2)*(pressure * (1-q) + pressureNear*math.pow((1-q), 2))
                     displacementVector = r_ij * displacementTerm
                     particle_B.position += displacementVector*0.5
                     particleADisplacement -= displacementVector*0.5
@@ -140,7 +146,8 @@ class VEFluid:
         for i in range(len(self.particles)):
             density = 0
             densityNear = 0
-            neighbours: List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(i)
+            neighbours: List[Data.Particle] = self.fluidHashGrid.getNeighboursOfParticleIdx(
+                i)
             particle_A = self.particles[i]
             pos_A = particle_A.position
 
@@ -167,35 +174,32 @@ class VEFluid:
                 if q < 1.0:
                     r_ij.normalize()
                     one_minus_q = 1 - q
-                    displacementTerm = (dt ** 2) * (pressure * one_minus_q + pressureNear * (one_minus_q ** 2))
+                    displacementTerm = (
+                        dt ** 2) * (pressure * one_minus_q + pressureNear * (one_minus_q ** 2))
                     displacementVector = r_ij * displacementTerm
                     particle_B.position += displacementVector * 0.5
                     particleADisplacement -= displacementVector * 0.5
 
             particle_A.position += particleADisplacement
 
-   
-
     def update(self, dt, mousePos):
         self.applyGravity(dt)
         self.predict_positions(dt)
-        #self.neighbourSearchB(mousePos)
+        # self.neighbourSearchB(mousePos)
         self.neighbourSearchC()
 
-        #double relaxation method
+        # double relaxation method
         self.doubleDensityRelaxationB(dt)
 
         self.boundary_collisions()
         self.compute_next_velocity(dt)
-
-
 
     def apply_forces(self, particle):
         pass
 
     def integrate(self, particle, dt):
         pass
-    
+
     def boundary_collisions(self):
         for p in self.particles:
             position = p.position
